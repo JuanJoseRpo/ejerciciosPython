524214a3252fe4cbd92c3eb46eb7d6eec41ab54c
JJ Rpo <juanjogithub@gmail.com>
2024-03-11 23:17:02 -0500

    ejerciciosNT se resuelve el ejercicio #1

---
+++ ejercicio1.py
@@ -0,0 +1,27 @@
+""" 
+Recibir un numero en teclado y determinar si este es múltiplo de 5
+Recibir un numero en teclado y determinar si este es múltiplo de 3
+
+ """
+
+numeroM5 = int(input(' Digita un numero para determinar si es multipo de 5: ')) 
+
+if numeroM5%5!=0:
+    print(f'{numeroM5} no es multiplo de 5')
+else:
+    print(f'{numeroM5} SI es multiplo de 5')   
+    
+print('-----------------------------------------------------------------------------')
+
+numeroM3 = int(input(' Digita un numero para determinar si es multipo de 3: ')) 
+if numeroM3%3!=0:
+    print(f'{numeroM3} no es multiplo de 3')
+else:
+    print(f'{numeroM3} SI es multiplo de 3')  
+
+
+         
+
+        
+
+    
